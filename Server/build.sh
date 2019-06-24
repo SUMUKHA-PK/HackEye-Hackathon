93b607c6a83745bd71e3db654e04849c1f95ad5d
SUMUKHA-PK <sumukhapk46@gmail.com>
2019-06-24 21:44:59 +0530

    Some basic routing done

---
+++ Server/build.sh
@@ -0,0 +1,1 @@
+go build -o main -v ./cmd/... && ./main
---
+++ Server/routing/authentication.go
@@ -0,0 +1,17 @@
+package routing
+
+import "net/http"
+// Login helps in login
+func Login(w http.ResponseWriter, r* http.Request)  {
+
+}
+
+// Register registers a user
+func Register(w http.ResponseWriter, r* http.Request)  {
+
+}
+
+// AuthenticateRequest auths every req
+func AuthenticateRequest(w http.ResponseWriter, r* http.Request)  {
+
+}
