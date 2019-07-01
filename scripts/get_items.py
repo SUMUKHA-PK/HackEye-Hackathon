4f35c84886b185f565aa49ceea5c7ffe051db19c
mahirjain25 <mahirjain25@gmail.com>
2019-07-01 18:09:59 +0530

    Added recipe names to dataset.

--- scripts/get_items.py
+++ scripts/get_items.py
@@ -43,6 +43,7 @@ def get_items(filepath,savefolder):
     data = pd.read_csv(filepath).dropna(subset=["features.value", "name"])
     print(data.head())
     recipes = data["features.value"].str.lower().tolist()
+    recipe_names = data["name"].tolist()
     # allergy_info, recipes = get_allergy_info(recipes)
     recipe_table=[]
     for recipe_no,recipe in enumerate(recipes):
@@ -52,11 +53,11 @@ def get_items(filepath,savefolder):
             for sep_items in items:
                 all_items.append(sep_items)
                 recipe_items.append(sep_items)
-        recipe_table.append((recipe_items,recipe_no))
+        recipe_table.append((recipe_items,recipe_no,recipe_names[recipe_no]))
     for item,recipe in zip(recipes[:10],recipe_table[:10]):
         print(item,recipe,sep="\n")
         print('-'*90)
-    pd.DataFrame(recipe_table,columns=["recipe","recipe_number"]).to_csv(savefolder+"/recipe_table.csv",index=False)
+    pd.DataFrame(recipe_table,columns=["recipe","recipe_number","recipe_name"]).to_csv(savefolder+"/recipe_table.csv",index=False)
     pd.DataFrame({'items':all_items}).to_csv(savefolder+"/items.csv")
 
 
